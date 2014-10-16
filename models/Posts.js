85481b280ab783d58266f832c9c53768a0c4cfa8
May's Mac <KUOM@MaysdeiMac.local>
2014-10-16 19:58:33 +0800

    finish 60

--- models/Posts.js
+++ models/Posts.js
@@ -7,4 +7,9 @@ var PostSchema = new mongoose.Schema({
 	comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
+PostSchema.method.upvote = function(cb) {
+	this.upvotes += 1;
+	this.save(db);
+}
+
 mongoose.model('Post', PostSchema);
